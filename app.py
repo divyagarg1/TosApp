af58e7e4e996ceb94b851c585aede667e2bc271b
Divya Garg <divya.garg@berkeley.edu>
2016-12-04 07:54:50 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,23 +11,15 @@ def main():
 def summarize1():
     _text =  request.form['inputText']
     if _text:
-		return test(_text)
+		return  json.dumps(summarizeAlgo(_text))
     else:
         return json.dumps({'html':'<span>Enter the text</span>'})
-
-def loadText(text):
-    f = open(text)
-    raw = f.read().decode('utf8')
-    return raw
 
 def summarizeAlgo(_text): 
 	try:
 		return summarize(_text, split=True, ratio=.05)
 	except:
 		return sys.exc_info()[0]
-
-def test(_text):
-	return json.dumps(summarizeAlgo(_text))
 
 if __name__ == "__main__":
     app.run()
