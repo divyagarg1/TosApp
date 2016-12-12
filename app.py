8aae7e6c53f78ae96f99ba2e903b8f0e1f618247
cagaray <cgaray@gmail.com>
2016-12-12 01:36:40 -0800

    aaa

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def main():
 def summarize1():
     _text =  request.form['inputText']
     if _text:
-        return  json.dumps(summarizeAlgo(_text))
+        #return  json.dumps(summarizeAlgo(_text))
+        return json.dumps({'html':'<span>Prueba</span>'})
     else:
         return json.dumps({'html':'<span>Enter the text</span>'})
 
